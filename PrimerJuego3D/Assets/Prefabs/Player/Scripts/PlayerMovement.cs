137d65d4b775e100ba0b7a17e058109262aa1234
GonzG1811 <basiliskco18@gmail.com>
2025-09-29 01:38:25 -0300

    Tuto3 completadoo

--- PrimerJuego3D/Assets/Prefabs/Player/Scripts/PlayerMovement.cs
+++ PrimerJuego3D/Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Player player;
+    private IMovementStrategy movementStrategy;
+
     private Vector3 forceToApply;
     private float timeSinceLastForce;
     private float intervalTime;
+
     private void Start()
     {
         forceToApply = new Vector3(0, 0, 10);
         timeSinceLastForce = 0f;
         intervalTime = 2f;
+
+        SetMovementStrategy(new SmoothMovement());
     }
 
     private void FixedUpdate()
@@ -17,8 +23,30 @@ public class PlayerMovement : MonoBehaviour
         timeSinceLastForce += Time.fixedDeltaTime;
         if (timeSinceLastForce >= intervalTime)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(forceToApply,ForceMode.Impulse);
+            gameObject.GetComponent<Rigidbody>().AddForce(forceToApply, ForceMode.Impulse);
             timeSinceLastForce = 0f;
         }
+
+        MovePlayer();
+    }
+
+    public void SetMovementStrategy(IMovementStrategy strategy)
+    {
+        movementStrategy = strategy;
+    }
+
+    public void MovePlayer()
+    {
+        movementStrategy.Move(transform, player);
+    }
+
+    public void SetSmoothMovement()
+    {
+        SetMovementStrategy(new SmoothMovement());
+    }
+
+    public void SetAccelerateMovement()
+    {
+        SetMovementStrategy(new AcelerateMovement());
     }
 }
